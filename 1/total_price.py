233e1e521f5db6c4292d5d3964e9a8661c73d6fe
danilo-santos <danilo.andrade333@gmail.com>
2025-07-21 20:25:13 -0300

    Exericicios de estudo da Escola da Nuvem

---
+++ 1/total_price.py
@@ -0,0 +1,5 @@
+product = 'Cadeira Infantil'
+price = 12.40
+quantity = 3
+
+print(f'Produto: {product}, Preço: {price}, Quantidade: {quantity}, Total: {price * quantity}')
---
+++ 1/volume.py
@@ -0,0 +1,6 @@
+height = 20
+width = 14
+depth = 12
+
+volume = height * width * depth
+print(volume)
